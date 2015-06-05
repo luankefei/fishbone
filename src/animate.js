78413074bb50067d9fdd08f80f35726587e1bc54
luankefei <luankefei@gmail.com>
2015-06-05 16:43:45 +0800

    add animate module

---
+++ src/animate.js
@@ -0,0 +1,34 @@
+/**
+ * @name animate.js
+ * @description 动画模块
+ * @date 2015.6.5
+ */
+
+
+var Animate = {}
+
+
+Animate.init = function(params, duration, easing, callback) {
+
+    // 这是fishbone对象
+    this
+    // 这是fishbone对象里面的dom数组
+    this.nodes
+
+
+
+
+
+    callback.call(this)
+}
+
+
+
+
+
+
+
+/**
+ * 2015.6.5
+ * 创建模块
+ */
