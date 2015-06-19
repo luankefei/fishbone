d2fc524553a74f51b085af574a7eda66191d2a52
luankefei <luankefei@gmail.com>
2015-06-19 16:45:45 +0800

    update animate module

--- src/animate.js
+++ src/animate.js
@@ -6,18 +6,43 @@
  */
 var Animate = {}
 
-// 返回初始值的数组
-//
+// 支持的变化属性，及默认单位
+Animate.supports = {
+    'width': 'px',
+    'height': 'px',
+    'top': 'px',
+    'left': 'px',
+    'right': 'px',
+    'bottom': 'px',
+    // 'transform': 'deg'
+}
 
-Animate.getBegin = function(key) {
+// 当前动画的key集合
+Animate.keys = []
+
+// 返回初始值的数组
+// TODO: getBegin应该能处理一组参数，数组里面存动画对象
+// TODO: 动画不精确，因为使用了float，最后一步要进行纠正，不能按照distance来走
+
+// 计算初始值
+Animate.getBegins = function(params) {
 
     var begins = []
 
+    // 遍历params进行遍历
     for (var i = 0; i < this.length; i++) {
 
-        var value = Number.parseFloat(this.eq(i).css(key))
+        var params = {}
+        var obj = this.eq(i)
 
-        begins.push(value)
+        for (var j = 0; j < Animate.keys.length; j++) {
+
+            var key = Animate.keys[j]
+
+            params[key] = Number.parseFloat(obj.css(key))
+        }
+
+        begins.push(params)
     }
 
     return begins
@@ -31,44 +56,91 @@ Animate.getDistance = function(begin, end, duration, frame) {
     // begin是数组，end是值
     for (var i = 0; i < begin.length; i++) {
 
-        var value = (end - begin[i]) / (duration / frame)
+        var changes = []
 
-        distances.push(value)
+        // begin是一个对象数组，需要遍历计算
+        for (var k in begin[i]) {
+
+            // 结束值 - 初始值 / 帧频
+            changes.push((Number.parseFloat(end[k]) - begin[i][k]) / (duration / frame))
+        }
+
+        distances.push(changes)
     }
 
     return distances
+}
+
+// 过滤初始值
+Animate.paramFilter = function(params) {
+
+    // 过滤params，同时补全单位
+    // 整理后的animate key
+    var keys = []
+
+    // 使用for in 遍历params
+    for (var k in params) {
+
+        if (!Animate.supports[k]) {
+
+            delete params[k]
+
+        } else {
+
+            keys.push(k)
+        }
+    }
+
+    Animate.keys = keys
+
+    return params
 }
 
 // 对外暴露的接口
 Animate.init = function(params, duration, callback) {
 
     var start = new Date()
-
     var target = this
 
     var step = 0,           // 当前步数
         fps = 60,           // fps
         times = null        // 循环次数
 
-    var duration = 1000     // 动画时间
-    var frame = 15          // magic number: 15毫秒为一帧
+    // var duration = 1000     // 动画时间
+    var frame = 13          // magic number: 15毫秒为一帧
+
+    // duration *= 0.85
+
+    // 过滤初始值
+    params = Animate.paramFilter(params)
 
     // TODO: 没有对param进行处理
-    var begin = Animate.getBegin.call(this, 'height'),
-        end = Number.parseFloat(params.height),
+    var begin = Animate.getBegins.call(this, params),
+        end = params,
         now = begin,
         distances = Animate.getDistance(begin, end, duration, frame)
 
+    // 动画执行次数
     times = Math.ceil(duration / frame)
 
+    // 记录当前动画的执行状态
+    this.data('isAnimated', true)
+
+    var stepStart = new Date()
+
+    // 执行动画
     var wait = setInterval(function() {
 
+        var currentFrame = new Date() - stepStart
+        stepStart = new Date()
+
+        // 最后一帧
         if (step === times) {
 
             var end = new Date() - start
 
             // 测试时间
-            // alert(end)
+            console.log('end time: ' + end)
 
             clearInterval(wait)
 
@@ -76,21 +148,60 @@ Animate.init = function(params, duration, callback) {
 
             for (var i = 0; i < target.length; i++) {
 
-                // TODO: 没有使用param
-                var height = now[i] + distances[i]
+                for (var j = 0; j < Animate.keys.length; j++) {
 
-                now[i] = height
-                height = height + 'px'
+                    var key = Animate.keys[j]
+                    var value = now[i][key] + distances[i][j]
 
-                target.eq(i).css('height', height)
+                    // 如果是最后一步，纠正误差
+                    if (step === times - 1) {
+
+                        value = params[key]
+                    }
+                
+                    var unit = Animate.supports[key]
+
+                    // 重置当前数值
+                    now[i][key] = value
+
+                    // 根据key处理value
+                    // 处理特殊值
+                    if (key === 'rotate') {
+
+                        key = 'transform'
+                        value = 'rotate(' + value + unit + ')'
+
+                        // TODO: 如果加入rotate、scale等，在getBegins和getDistance里面，也要做特殊处理
+                        // console.log('rotate')
+                        // console.log(key)
+                        // console.log(value)
+                        // console.log(now[i][key])
+                        // console.log(distances[i][j])
+                    // 处理常规值：width、height、left、top、bottom、right
+                    } else {
+
+                        // 补上单位
+                        value = value + unit    
+                    }
+
+                    
+
+                    target.eq(i).css(key, value)
+                }
             }
 
             // 步数 + 1
             step = step + 1
         }
 
+        var stepStop = new Date - stepStart
+
     }, frame)
+
+    return this
 }
+
+
 
 /*
 
@@ -100,8 +211,6 @@ Animate.linear = function(t, b, c, d) {
 }
 
 Animate.init = function(params, duration, callback) {
-
-    var sss = new Date()
 
     var ele = this;
 
@@ -136,8 +245,6 @@ Animate.init = function(params, duration, callback) {
                 ele.css(attr, params[attr] + unit[attr])
             }
 
-            alert(new Date - sss)
-
             clearInterval(ele.timer);
             ele.timer = null;
             if (typeof callback == "function") {
@@ -156,5 +263,6 @@ Animate.init = function(params, duration, callback) {
  * 2015.6.5
  * 创建模块
  * 2015.6.15 重写动画模块
+ * 2015.6.18 初步测试可用，与上个版本功能一致
  */
  
